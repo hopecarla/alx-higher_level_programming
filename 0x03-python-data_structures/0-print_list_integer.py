376e5247ff5e437ebf1b4565156a122d827fc14c
hopecarla <hopememba.carla@gmail.com>
2023-12-01 03:24:48 -0800

    data

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for n in my_list:
+        print("{:d}".format(n)
+
+        if __name__ == "_main__":
+        my_list = [1, 2, 4, 4, 5]
+        print_list_integer(my_list)
