75a0f9a8994c9a2994d68a0f2e569501962ddfc2
hopecarla <hopememba.carla@gmail.com>
2023-12-04 00:40:28 -0800

    6-print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for r in matrix:
         for i in range(len(r)):
-            print("{:d}".format(r[i], end="")
-                    if i < len(r) - 1:
-                    print(" ", end="")
-                    print()
+            print("{:d}".format(r[i]), end="")
+            if i < len(r) - 1:
+                print(" ", end="")
+        print()
