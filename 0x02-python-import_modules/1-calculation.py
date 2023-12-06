52711ac77e848745fe0a6cff6cb3e3fea4868ddf
hopecarla <hopememba.carla@gmail.com>
2023-12-06 12:50:01 -0800

    1-calculation

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+
+result_add = (a, b)
+result_sub = (a, b)
+result_mul = (a, b)
+result_div = (a, b)
+
+print("Result of addition:", result_add)
+print("Result of subtraction:", result_sub)
+print("Result of multiplication:", result_mul)
+print("Result of division:", result_div)
