5fafe9b33b639a77f0a2863b9c63596aff16b1fa
hopecarla <hopememba.carla@gmail.com>
2023-12-06 13:35:42 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ def print_arguments(argv):
     if num_args == 0:
         print("0 arguments: .")
     else:
-        print(f"{num_args} arguments:{'argument' if num_args == 1 else 'arguments'}:")
+        print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
 
         for i, arg in enumerate(argv, start=1):
             print(f"{i}: {arg}")
