fc75a8e77396f786a5775cd0748fa884ff76fe87
hopecarla <hopememba.carla@gmail.com>
2023-12-06 13:29:13 -0800

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+def print_arguments(argv):
+    num_args = len(argv)
+
+    if num_args == 0:
+        print("0 arguments: .")
+    else:
+        print(f"{num_args} arguments:{'argument' if num_args == 1 else 'arguments'}:")
+
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
+
+if __name__ == "__main__":
+    print_arguments(sys.argv[1:])
