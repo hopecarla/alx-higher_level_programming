af496d908a20086a499e972cb7a625e794e8cb6d
hopecarla <hopememba.carla@gmail.com>
2023-12-21 03:23:16 -0800

    4-square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Defines a class."""
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size=0):
+        """Initializes a new instance of the Square class.
+
+        Args:
+        size (int): The size of the square, default is 0.
+
+        """
+        self.__size = size
+
+        @property
+        def size(self):
+            """Get the size of the square."""
+            return self.__size
+
+        @size_setter
+        def size(self, value):
+            """Set the size of the square."""
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+            def area(self):
+                """Calculate and return the area of the square."""
+                return self.__size ** 2
+
