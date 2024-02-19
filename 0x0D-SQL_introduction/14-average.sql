10eefd599fa26d3379d3d4beef3aa11a465a24b4
hopecarla <hopememba.carla@gmail.com>
2024-02-19 04:14:16 -0800

    sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -2,5 +2,6 @@
 -- Script to compute the score average of all records
 
 -- Compute the average score of all records
-SELECT AVG(`score`) AS `average'
+SELECT AVG(`score`) AS `average`
 FROM `second_table`;
+
